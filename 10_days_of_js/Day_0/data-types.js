be1490a3970dcbeff0b9134e69fecc7fbdd359b5
axatbhardwaj-solulab <40573916+axatbhardwaj@users.noreply.github.com>
2021-07-19 17:43:06 +0530

    10_days_of_js

---
+++ 10_days_of_js/Day_0/data-types.js
@@ -0,0 +1,9 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  const firstDecimal = 4.0;
+  const firstString = "HackerRank ";
+
+  console.log(firstInteger + parseInt(secondInteger, 0));
+  console.log(firstDecimal + parseFloat(secondDecimal, 2));
+  console.log(firstString + secondString);
+}
